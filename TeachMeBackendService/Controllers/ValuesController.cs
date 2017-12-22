5768964df1465dab8981cfae359e4d921bee7ca5
bin0m <levitg@gmail.com>
2017-12-22 20:34:40 +0300

    Added API Custom Controller for Sections, which supports delete section with all lessons inside

--- TeachMeBackendService/Controllers/ValuesController.cs
+++ TeachMeBackendService/Controllers/ValuesController.cs
@@ -1,7 +1,12 @@
 ﻿using System.Web.Http;
 using System.Web.Http.Tracing;
+using System.Linq;
+using System.Data;
+using System.Data.Common;
+using System.Data.Entity;
 using Microsoft.Azure.Mobile.Server;
 using Microsoft.Azure.Mobile.Server.Config;
+using TeachMeBackendService.DataObjects;
 
 namespace TeachMeBackendService.Controllers
 {
@@ -27,6 +32,46 @@ namespace TeachMeBackendService.Controllers
         public string Post()
         {
             return "Hello World!";
+        }
+
+        // GET api/values
+        public IHttpActionResult GetValueById(string id)
+        {
+            Section section = null;
+
+            using (var ctx = new Models.TeachMeBackendContext())
+            {
+                section = ctx.Sections.Find(id);
+            }
+
+            if (section == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(section);
+        }
+
+
+        // Delete api/values
+        public IHttpActionResult Delete(string id)
+        {
+            using (var dbContext = new Models.TeachMeBackendContext())
+            {
+                //var section = dbContext.Sections.Find(id);
+                var section = dbContext.Sections.Include(c => c.Lessons).FirstOrDefault(c => c.Id == id);
+
+                if (section == null)
+                {
+                    return NotFound();
+                }
+                
+                dbContext.Sections.Remove(section);
+                dbContext.SaveChanges();
+                          
+            }
+
+            return Ok();
         }
     }
 }
---
+++ TeachMeBackendService/ControllersAPI/SectionsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+using TeachMeBackendService.DataObjects;
+using TeachMeBackendService.Models;
+
+namespace TeachMeBackendService.ControllersAPI
+{
+
+    [MobileAppController]
+    public class SectionsController : ApiController
+    {
+        private TeachMeBackendContext db = new TeachMeBackendContext();
+
+        // GET: api/Sections
+        public IQueryable<Section> GetSections()
+        {
+            return db.Sections;
+        }
+
+        // GET: api/Sections/5
+        [ResponseType(typeof(Section))]
+        public IHttpActionResult GetSection(string id)
+        {
+            Section section = db.Sections.Find(id);
+            if (section == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(section);
+        }
+
+        // PUT: api/Sections/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSection(string id, Section section)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != section.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(section).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SectionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Sections
+        [ResponseType(typeof(Section))]
+        public IHttpActionResult PostSection(Section section)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Sections.Add(section);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (SectionExists(section.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = section.Id }, section);
+        }
+
+        // DELETE: api/Sections/5
+        [ResponseType(typeof(Section))]
+        public IHttpActionResult DeleteSection(string id)
+        {
+            using (var dbContext = new Models.TeachMeBackendContext())
+            {
+                //TODO: use faster query using PK Index: dbContext.Sections.Find(id);
+                var section = dbContext.Sections.Include(c => c.Lessons).FirstOrDefault(c => c.Id == id);
+
+                if (section == null)
+                {
+                    return NotFound();
+                }
+
+                dbContext.Sections.Remove(section);
+                dbContext.SaveChanges();
+
+            }
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SectionExists(string id)
+        {
+            return db.Sections.Count(e => e.Id == id) > 0;
+        }
+    }
+}
