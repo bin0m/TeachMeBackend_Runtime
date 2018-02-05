e05c900ca47745afee3103ee9f213f8d4ef4de3a
bin0m <levitg@gmail.com>
2018-02-05 18:43:50 +0300

    Added API function ../api/lessons/{id}/exercises- which returns exercises by LessonId

---
+++ TeachMeBackendService/ControllersAPI/ExercisesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+using Microsoft.Web.Http;
+using TeachMeBackendService.DataObjects;
+using TeachMeBackendService.Models;
+
+namespace TeachMeBackendService.ControllersAPI
+{
+    [ApiVersion("1.0")]
+    [RoutePrefix("api/v{version:ApiVersion}/exercises")]
+    [MobileAppController]
+    public class ExercisesController : ApiController
+    {
+        private TeachMeBackendContext db = new TeachMeBackendContext();
+
+        // GET: api/Exercises
+        [Route("")]
+        public IQueryable<Exercise> GetExercises()
+        {
+            return db.Exercises;
+        }
+
+        // GET: api/Exercises/5
+        [Route("{id}")]
+        [ResponseType(typeof(Exercise))]
+        public IHttpActionResult GetExercise(string id)
+        {
+            Exercise exercise = db.Exercises.Find(id);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(exercise);
+        }
+
+        
+        [Route("~/api/v{version:ApiVersion}/lessons/{id}/exercises")]
+        public IQueryable<Exercise> GetBySection(string id)
+        {
+            var exercises = db.Exercises.Where(c => c.LessonId == id);
+
+            return exercises;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ExerciseExists(string id)
+        {
+            return db.Exercises.Count(e => e.Id == id) > 0;
+        }
+    }
+}
