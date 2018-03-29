40251ecb59a4e6f9540b16aaf785b647189ccfb5
bin0m <levitg@gmail.com>
2018-03-29 21:52:46 +0300

    minor fixes

--- TeachMeBackendService/App_Start/IdentityConfig.cs
+++ TeachMeBackendService/App_Start/IdentityConfig.cs
@@ -8,7 +8,6 @@ using TeachMeBackendService.Models;
 namespace TeachMeBackendService
 {
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
