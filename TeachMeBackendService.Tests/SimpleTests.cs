f08d7fbe4d555cbc52a798201ccac3bae19ecdad
bin0m <levitg@gmail.com>
2018-08-20 18:48:03 +0300

    Add NUnit test project

---
+++ TeachMeBackendService.Tests/SimpleTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TeachMeBackendService;
+
+namespace TeachMeBackendService.Tests
+{
+       [TestFixture]
+        public class SimpleTests
+    {
+            private readonly ControllersTables.ValuesController valuesController;
+
+            public SimpleTests()
+            {
+                valuesController = new ControllersTables.ValuesController();
+            }
+
+            [Test]
+            public void ReturnFalseGivenValueOf1()
+            {
+                var result = valuesController.Get();
+
+                Assert.AreEqual(result, "Hello from localhost");
+            }
+        }
+   
+}
