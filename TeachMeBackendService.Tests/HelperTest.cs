46795976627f6787ba91db962bcfa26cdd5a4e96
bin0m <levitg@gmail.com>
2018-08-21 16:45:13 +0300

    Add Base64EncodeDecodeTest1

--- TeachMeBackendService.Tests/HelperTest.cs
+++ TeachMeBackendService.Tests/HelperTest.cs
@@ -6,7 +6,14 @@ namespace TeachMeBackendService.Tests
     [TestFixture]
     public class HelperTest
     {
-
-       
+        [Test]
+        public void Base64EncodeDecodeTest1()
+        {
+            string data = "myData12";
+            string encoded = Helper.Base64Encode(data);
+            Assert.AreNotEqual(data, encoded);
+            string decoded = Helper.Base64Decode(encoded);
+            Assert.AreEqual(data, decoded);
+        }
     }
 }
