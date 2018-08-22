2faca5267331e1d45cdcb41374b04038405a4155
bin0m <levitg@gmail.com>
2018-08-22 18:48:00 +0300

    Adding tests

--- TeachMeBackendService.Tests/HelperTest.cs
+++ TeachMeBackendService.Tests/HelperTest.cs
@@ -15,5 +15,15 @@ namespace TeachMeBackendService.Tests
             string decoded = Helper.Base64Decode(encoded);
             Assert.AreEqual(data, decoded);
         }
+
+        [Test]
+        public void Base64EncodeDecodeTest2()
+        {
+            string data = "H#J^621)(*&!^>+?=";
+            string encoded = Helper.Base64Encode(data);
+            Assert.AreNotEqual(data, encoded);
+            string decoded = Helper.Base64Decode(encoded);
+            Assert.AreEqual(data, decoded);
+        }
     }
 }
